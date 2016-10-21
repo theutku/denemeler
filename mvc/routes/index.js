edd057180d8d248b010780175d68eaeddc76d73c
Utku TURKOGLU <uturkoglu@cinerpark.local>
2016-10-21 17:23:50 +0300

    MVC App v1 Complete

--- mvc/routes/index.js
+++ mvc/routes/index.js
@@ -4,11 +4,68 @@ var express = require('express');
 var router = express.Router();
 
 
+//Require Guitarists Data ==============
+
+var appdata = require('../data.json');
+
 //Get HomePage ==========================
 
 router.get('/', function(req, res) {
+  var myArtwork = [];
+  var myGuitarists = appdata.guitarists;
+
+  myGuitarists.forEach(function(item) {
+    myArtwork = myArtwork.concat(item.artwork);
+  });
   res.render('index', { 
-      title: 'Express' });
+      title: 'Top Guitarists',
+      artwork: myArtwork,
+      guitarists: myGuitarists,
+      page: 'home' 
+    });
 });
+
+
+//GET Guitarists Page ==================
+
+router.get('/guitarists', function(req, res) {
+  var myArtwork = [];
+  var myGuitarists = appdata.guitarists;
+
+  appdata.guitarists.forEach(function(item) {
+    myArtwork = myArtwork.concat(item.artwork);
+  });
+  res.render('guitarists', { 
+      title: 'Guitarists',
+      artwork: myArtwork,
+      guitarists: myGuitarists,
+      page: 'guitaristList' 
+    });
+});
+
+
+//GET Specific Guitarists Page ==================
+
+router.get('/guitarists/:guitaristid', function(req, res) {
+  var myArtwork = [];
+  var myGuitarists = [];
+
+  appdata.guitarists.forEach(function(item) {
+    if(item.shortname == req.params.guitaristid) {
+      myGuitarists.push(item);
+      myArtwork = myArtwork.concat(item.artwork);
+
+    }
+  });
+  res.render('guitarists', { 
+      title: 'Guitarists',
+      artwork: myArtwork,
+      guitarists: myGuitarists,
+      page: 'guitarist' 
+    });
+});
+
+
+//Export Module ================================
 
 module.exports = router;
