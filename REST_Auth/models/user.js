b435c2a031ab8516e6eda91a68d7a6e14a29846c
Utku TURKOGLU <uturkoglu@cinerpark.local>
2016-10-25 16:33:35 +0300

    complete up to passport

---
+++ REST_Auth/models/user.js
@@ -0,0 +1,35 @@
+//Require Modules ====================================
+
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+
+//User Schema ========================================
+
+var UserSchema = mongoose.Schema({
+    username: {
+        type: String,
+        index: true
+    },
+    password: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    password: {
+        type: String
+    }
+});
+
+var User = module.exports = mongoose.model('User', UserSchema);
+
+//Create User and Pass Hash ==========================
+
+module.exports.createUser = function(newUser, callback) {
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(newUser.password, salt, function(err, hash) {
+            newUser.password = hash;
+            newUser.save(callback);
+        })
+    })
+}
--- REST_Auth/routes/users.js
+++ REST_Auth/routes/users.js
@@ -3,11 +3,14 @@
 var express = require('express');
 var router = express.Router();
 
-//Register =============================================
+var User = require('../models/user');
+
+//Register Page ========================================
 
 router.get('/register', function(req, res) {
     res.render('register', {
-        title: 'Register - AuthApp'
+        title: 'Register - AuthApp',
+        errors: []
     });
 
 });
@@ -19,6 +22,55 @@ router.get('/login', function(req, res) {
         title: 'Login - AuthApp'
     });
 
+});
+
+//Register New User ====================================
+
+router.post('/register', function(req, res) {
+    var name = req.body.name;
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+    
+    //Validation =======================================
+    req.checkBody('name', 'Name is required').notEmpty();
+    req.checkBody('username', 'Username is required').notEmpty();
+    req.checkBody('email', 'E - mail is required').notEmpty();
+    req.checkBody('email', 'E - mail is not valid').isEmail();
+    req.checkBody('password', 'Password is required').notEmpty();
+    req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+    
+    var errors = req.validationErrors();
+      
+    if(errors) {
+        res.render('register', {
+            title: 'Register - Error',
+            errors: errors
+        });
+    } else {
+        var newUser = new User({
+            name: name,
+            username: username,
+            email: email,
+            password: password
+        });
+
+        //Pass user to model =============================
+        User.createUser(newUser, function(err, user){
+            if(err) {
+                console.log(err);
+                throw err;
+            } else {
+                console.log(user);
+            }
+        });
+
+        //Success Message and Redirect ===================
+        req.flash('success_msg', 'You are now registered and can now login!');
+        res.redirect('/users/login');
+    }
+    
 });
 
 module.exports = router;
