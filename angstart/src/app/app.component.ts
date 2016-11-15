18242f0c12766202349a4c128826bd6c5811fd37
Utku TURKOGLU <uturkoglu@cinerpark.local>
2016-11-15 16:51:27 +0200

    angular2 learn in prg

--- angstart/src/app/app.component.ts
+++ angstart/src/app/app.component.ts
@@ -3,10 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
-  title: string = 'app works!';
-  parg: string = 'Displaying the paragraph too!'
 }
--- angstart/src/app/app.module.ts
+++ angstart/src/app/app.module.ts
@@ -4,12 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { OtherComponent } from './other/other.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OtherComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
