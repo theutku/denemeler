2edab3a35440e5a41deba5eb61258240453cdafe
Utku TURKOGLU <uturkoglu@cinerpark.local>
2016-11-17 16:54:02 +0200

    ang1 tryouts

--- angstart/src/app/app.module.ts
+++ angstart/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipelistComponent } from './recipes/recipelist/recipelist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HeaderComponent,
+    RecipesComponent,
+    RecipelistComponent
   ],
   imports: [
     BrowserModule,
