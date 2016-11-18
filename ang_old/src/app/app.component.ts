edd653b7dd6f096f82c8f287ccad4d692fdfb9a7
Utku Utku <utku2107@Utkus-MacBook-Air.local>
2016-11-18 23:57:07 +0300

    angular trials

--- ang_old/src/app/app.component.ts
+++ ang_old/src/app/app.component.ts
@@ -1,22 +1,9 @@
 import { Component } from '@angular/core';
 
-export class Hero {
-  id: number;
-  name: string;  
-}
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HEROES } from './mock-heroes'
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
@@ -27,6 +14,7 @@ const HEROES: Hero[] = [
 export class AppComponent {
   title = 'Tour of Heroes';
   selectedHero: Hero;
+  
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
