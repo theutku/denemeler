93f4254fc3f56f5c90e0ef1c864eac413d5ea0f2
Utku TURKOGLU <uturkoglu@cinerpark.local>
2017-01-16 11:43:53 +0300

    router fix

--- tsrest/src/api.ts
+++ tsrest/src/api.ts
@@ -30,6 +30,9 @@ class ApiApp {
         });
 
 
+        //this.app.use('/web', RouteLoader.init(this.router));
+        var webRoutes = express.Router();
+        this.app.use('/web', RouteLoader.init(webRoutes));
 
         this.app.use((req, res, next) => {
             res.status(404).send('404 Not Found');
--- tsrest/src/routes/index.ts
+++ tsrest/src/routes/index.ts
@@ -3,7 +3,7 @@ import * as express from 'express';
 export default class RouteLoader {
     public static init(router?: express.Router) {
         return [
-            require('./web').default.init(router)
+            require('./web').default(router)
         ]
     }
 }
